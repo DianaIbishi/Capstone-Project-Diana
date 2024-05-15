706bd16fdb6ae82528ec1463fa86036f8d30310a
Diana Ibishi <diana.ibishi@stud.unilu.ch>
2024-05-15 12:57:04 +0200

    WD

---
+++ Capstone Project R Script.R
@@ -0,0 +1,40 @@
+# The aim of this capstone project is to compare extreme weather differences in the region of
+# Kosovo with the number of protests. 
+
+# I want to explain what I am going to analyze in this paper.
+
+# As explained in the last few paragraphs climate change has a strong impact on Kosovo and 
+# the government does not really care about the environmental rules (as it did not sign neither
+# the UN Framework Convention on Climate Change (UNFCCC) nor the Kyoto Protocol) or permits. 
+# So, I was wondering why in Kosovo no protest climate change take place. As for example 
+# climate change has a big impact on the citizens personal life.
+
+# For this project I follow a Hypothesis:
+
+# "Regions experiencing climatic stress are more likely to not protest the government."
+
+# To compare this I will use an API which describes 80 years of weather history data.
+
+# For the number of protests I am going to use the ACLED | Armed Conflict Location & 
+# Events Data set. 
+
+# In the end, I will test if my hypothesis is significant and if there is a correlation 
+# between the climatic stress (weather data) and the protests.
+
+
+
+
+# Set Working directory and check it with getwd()
+
+setwd("C:/Users/diana/Documents/Uni_Luzern/Master Computational Social Science/2. Semester/Data Mining with R/Capstone-Project-Diana")
+
+getwd()
+
+# precipitation, humidity, temperature (hotter summers), snow (winters with little snow) 
+
+
+
+
+
+
+
