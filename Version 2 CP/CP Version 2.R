2435d60ab4d198b52b9bc1ac588d02802477e9a9
Diana Ibishi <diana.ibishi@stud.unilu.ch>
2024-05-22 19:19:01 +0200

    Could do my first analysis

--- Version 2 CP/CP Version 2.R
+++ Version 2 CP/CP Version 2.R
@@ -61,6 +61,77 @@ print(response)
 df_kosovo <- response
 
 
+# Clean the data for missing values
+
+# Check for missing values in the entire dataframe
+
+any_na <- any(is.na(df_kosovo))
+any_na # There is no missing value - great
+
+
+# I remove the columns which I do not need
+
+library(dplyr)
+
+kosovo_cleaned_data <- select(df_kosovo, -event_id_cnty, -source, -admin3, -latitude,
+                              -longitude, -fatalities, -tags, -timestamp, -location,
+                              -admin2)
+
+View(kosovo_cleaned_data)
+
+
+# I rename the column admin1 which indicated the city
+
+kosovo_cleaned_data <- kosovo_cleaned_data %>%
+  rename(city = admin1)
+
+
+# Check the average of interactions in the data set
+
+summary_interactions <- kosovo_cleaned_data %>%
+  group_by(city) %>%
+  summarise(average_interaction = mean(interaction, na.rm = TRUE))
+  
+View(summary_interactions)
+
+# Order the average interactions per city 
+
+ordered_interactions <- summary_interactions[order(-summary_interactions$average_interaction), ]
+
+# Print the ordered dataframe
+
+print(ordered_interactions)
+
+# From 2018 until 2023 we see that the most interactions, so the different types of events
+# hat Pristina. I think this is cleas, as it is the capital city of Kosovo. 
+
+
+# Now I want to see how the interactions evolved with the years in the country
+
+summary_yearly_interactions <- kosovo_cleaned_data %>%
+  group_by(year) %>%
+  summarise(average_yearly_interaction = mean(interaction, na.rm = TRUE))
+
+View(summary_yearly_interactions)
+
+
+# Let's visualize this
+
+# Assuming you have the 'summary_yearly_interactions' dataframe
+
+hist(summary_yearly_interactions$year,
+     main = "Average Yearly Interactions",
+     xlab = "Years",
+     ylab = "Average Interaction",
+     col = "skyblue",
+     border = "darkblue")
+
+
+
+
+
+
+
 
 
 
