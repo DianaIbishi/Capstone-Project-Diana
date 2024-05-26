2c4c8feb9faf11c80366416122333534bb4f2540
Diana Ibishi <diana.ibishi@stud.unilu.ch>
2024-05-26 22:22:18 +0200

    Last Analysis

--- Version 2 CP/CP Version 2 Analysis.R
+++ Version 2 CP/CP Version 2 Analysis.R
@@ -33,8 +33,10 @@ summary(model)$coefficient
 
 # Two coefficients are very significant: 1. Peaceful protest (2.4855) and 2. Attack (1.1972)
 
-# So here I can interpret that the more protests happen in Kosovo which are either 
+# So here I could interpret that the more protests happen in Kosovo which are either 
 # peaceful or attacks the more this influences the average interaction of protest per year.
+# But of course is this the case, as the more protests there are the more interaction we have.
+
 
 # Now I want to test how much the error rate of the model is:
 
@@ -43,25 +45,18 @@ sigma(model)/mean(merged_df_kosovo$average_yearly_interaction)
 # The result is: 0.03268 -> The error rate of this model is quite low: 3.268 % - great 
 
 
-
-
-
-
 # Diagnostic tests
-# For example, you can check multicollinearity using VIF
+
+# For example, I can check multicollinearity using VIF
+
 vif(model)
 
-# You can also perform other diagnostic tests like heteroscedasticity, normality of residuals, etc.
-# For example, Breusch-Pagan test for heteroscedasticity
-bptest(model)
+# year : GVIF of 1.08 which is very low and doesn't show any multicollinearity
+# sub_event_type : GVIF of 1.12 which is very low and doesn't show any multicollinearity
+# protest_counts : GVIF of 4.71 which is a moderate level of multicollinearity
 
-
-
-
-
-
-
-
+# protest_counts needs a further investigation because it is not above >5 but there could
+# be a multicollinearity between the variable protest_counts and the interactions
 
 
 
